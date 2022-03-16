70b6070925007c7082c47ada00227556ca717091
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-16 12:31:09 -0600

    Directories updated.

--- server/server.js
+++ server/server.js
@@ -6,10 +6,21 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/",function(req, res){
-    res.sendFile(path.join(__dirname,"../index.html"))
+//middleware
+
+
+// endpoints
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "../public"))
 })
 
+app.get("/",function(req, res){
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+app.get("/styles", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/index.css"))
+})
 const port = process.env.PORT || 4200
 
 app.listen(port, () => {
